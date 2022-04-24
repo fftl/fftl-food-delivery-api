e8f44353d25330c88e895b1feb860f25353536f8
fftl <fftl.0710@gmail.com>
2022-04-24 23:55:41 +0900

    update: member entity

--- src/main/java/fftl/fooddeliveryapi/member/entity/Member.java
+++ src/main/java/fftl/fooddeliveryapi/member/entity/Member.java
@@ -4,6 +4,7 @@ import fftl.fooddeliveryapi.address.entity.MemberAddress;
 import fftl.fooddeliveryapi.order.entity.Order;
 import fftl.fooddeliveryapi.review.entity.Review;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.List;
@@ -14,13 +15,21 @@ public class Member {
     @Id
     private Long userId;
 
+    @Column
     private String username;
+    @Column
     private String password;
 
+    @Column
     private String nickname;
+    @Column
     private Boolean removed;
 
+    @Column
     private List<Order> orders;
+    @Column
     private List<MemberAddress> memberAddresses;
+    @Column
     private List<Review> reviews;
+
 }
