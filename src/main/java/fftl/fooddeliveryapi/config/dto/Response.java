99bac4722f5bbb94006832c99bff02e50ef9a62b
fftl <fftl.0710@gmail.com>
2022-04-27 11:11:23 +0900

    update: response dto

--- src/main/java/fftl/fooddeliveryapi/config/dto/Response.java
+++ src/main/java/fftl/fooddeliveryapi/config/dto/Response.java
@@ -5,5 +5,10 @@ public class Response<T> {
     private boolean success;
     private String message;
     private T data;
-    
+
+    public Response(boolean success, String message, T data){
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
 }
--- src/main/java/fftl/fooddeliveryapi/member/controller/MemberController.java
+++ src/main/java/fftl/fooddeliveryapi/member/controller/MemberController.java
@@ -1,12 +1,10 @@
 package fftl.fooddeliveryapi.member.controller;
 
+import fftl.fooddeliveryapi.config.dto.Response;
 import fftl.fooddeliveryapi.member.dto.SaveMemberRequest;
 import fftl.fooddeliveryapi.member.entity.Member;
 import fftl.fooddeliveryapi.member.service.MemberService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +18,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
-    public ResponseEntity<Member> saveMember(@RequestBody SaveMemberRequest saveMemberRequest){
+    public Response saveMember(@RequestBody SaveMemberRequest saveMemberRequest){
         Member member = memberService.saveMember(saveMemberRequest);
-        return new ResponseEntity<Member>(member, HttpStatus.CREATED);
+        return new Response(true, null, member);
     }
 }
